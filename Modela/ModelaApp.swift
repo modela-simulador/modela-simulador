fec55def3ebca2615ef215cf0a76f432083c492a
Sebastian <srplaza@uc.cl>
2026-02-26 17:56:24 -0300

    Initial Commit

---
+++ Modela/ModelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelaApp.swift
+//  Modela
+//
+//  Created by Sebastian Plaza santis on 26-02-26.
+//
+
+import SwiftUI
+
+@main
+struct ModelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
